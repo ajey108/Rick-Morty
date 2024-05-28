e1b7ec5cc686df8fc2c06582da085422a9c646a8
ajey108 <ajayxa1@gmail.com>
2024-05-28 12:55:56 +0530

    added Pagination

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
-const Pagination = () => {
+const Pagination = ({pageNo,setPageNo}) => {
+
+  const next=()=>{
+    setPageNo((x)=>x+1);
+  }
+
+  const prev=()=>{
+    if(pageNo === 1) return;
+    setPageNo((x)=> x-1);
+  }
+
+
   return (
-    <div>Pagination</div>
+    <div className="container d-flex justify-content-center my-4 gap-4">
+    <button  onClick={prev} className="btn btn-primary">Prev</button>
+    <button  onClick={next}  className="btn btn-primary">Next</button>
+    </div>
   )
 }
 
