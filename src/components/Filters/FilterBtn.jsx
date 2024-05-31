241de319cf72435987e6bb017ce73ec245265e55
ajey108 <ajayxa1@gmail.com>
2024-05-31 07:29:54 +0530

    update pagination

--- src/components/Filters/FilterBtn.jsx
+++ src/components/Filters/FilterBtn.jsx
@@ -1,49 +1,42 @@
 import React, { useState } from 'react';
 
-const FilterBtn = ({name,index,items,task,setPageNo}) => {
-  const [selectedOption, setSelectedOption] = useState('flexRadioDefault1');
+const FilterBtn = ({ name, index, items, task, setPageNo }) => {
+  const [selectedOption, setSelectedOption] = useState(null);
 
   const handleChange = (event) => {
     setSelectedOption(event.target.id);
+    setPageNo(1);
+    task(items);
   };
 
- 
   return (
     <div>
-  <style jsx>
-{`
-  .x:checked + label{
-    background-color:#0b5ed;
-    color:white;
-  }
-  input[type="radio"] {
-    display: none;
-  }
-`}
-</style>
+      <style jsx>{`
+        .x:checked + label {
+          background-color: #0b5ed7;
+          color: white;
+        }
+        input[type="radio"] {
+          display: none;
+        }
+      `}</style>
 
       <div className="form-check">
-        <input 
-        onClick={()=>{
-          setPageNo(1);
-          task(items);
-
-
-        }}  
-          className="form-check-input x" 
-          type="radio" 
-          name={name} 
+        <input
+          onClick={handleChange}
+          className="form-check-input x"
+          type="radio"
+          name={name}
           id={`${name}-${index}`}
-          checked={selectedOption === 'flexRadioDefault1'}
-          onChange={handleChange} 
+          checked={selectedOption === `${name}-${index}`}
+          onChange={handleChange}
         />
         <label className="btn btn-outline-primary" htmlFor={`${name}-${index}`}>
-         {items}
+          {items}
         </label>
       </div>
-      
     </div>
   );
-}
+};
 
 export default FilterBtn;
--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,23 +1,24 @@
-import React from 'react'
+import React from 'react';
 import ReactPaginate from 'react-paginate';
+import styles from './textwhite.module.scss';
 
-const Pagination = ({pageNo,setPageNo,info}) => {
-
-  
-
+const Pagination = ({ pageNo, setPageNo, info }) => {
   return (
-   <ReactPaginate  className="pagination justify-content-center gap-4 my-4" 
-   nextLabel = "Next"
-   previousLabel = "Prev"
-   nextclassNameName='btn btn-primary'
-   previousclassNameName='btn btn-primary'
-   pageclassNameName='page-item'
-   pageLinkclassNameName='page-link'
-   activeclassNameName='active'
-   forcePage={pageNo ===1 ? 0 : pageNo -1}
-   onPageChange={(data)=>{setPageNo(data.selected + 1)}}
-   pageCount={info?.pages}/>
-  )
-}
+    <ReactPaginate
+      className={`pagination justify-content-center gap-4 my-4 ${styles.textWhite}`}
+      nextLabel="Next"
+      previousLabel="Prev"
+      nextClassName={`btn btn-primary ${styles.textWhite}`}
+      previousClassName={`btn btn-primary ${styles.textWhite}`}
+      pageClassName="page-item"
+      pageLinkClassName={`page-link ${styles.textWhite}`}
+      activeClassName="active"
+      forcePage={pageNo === 1 ? 0 : pageNo - 1}
+      onPageChange={(data) => setPageNo(data.selected + 1)}
+      pageCount={info?.pages}
+    />
+  );
+};
 
-export default Pagination
+export default Pagination;
+
